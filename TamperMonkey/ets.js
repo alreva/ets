f6f493324839035d2ea39f507e097d84ec810b6c
alreva@gmail.com <ise6Songi>
2014-07-15 16:48:09 +0300

    Added require.js and the main file

---
+++ TamperMonkey/ets.js
@@ -0,0 +1,9 @@
+﻿// ==UserScript==
+// @name       ETS CR reporting improvement
+// @namespace  http://timeserver
+// @version    0.1
+// @description  enter something useful
+// @match      http://timeserver/accountreport.ets
+// @copyright  2014+, alreva
+// ==/UserScript==
+
