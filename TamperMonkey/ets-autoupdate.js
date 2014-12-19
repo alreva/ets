11fe0cf94a88f819133b7607840985a5e83b47a4
alreva@gmail.com <ise6Songi>
2014-12-19 17:21:03 +0200

    Did autoupdate

---
+++ TamperMonkey/ets-autoupdate.js
@@ -0,0 +1,18 @@
+﻿// ==UserScript==
+// @name       TFS to ETS Autoupdate
+// @namespace  http://timeserver.i.sigmasoftware.com
+// @version    0.1
+// @match      http://timeserver/accountreport.ets
+// @match      https://timeserver/accountreport.ets
+// @match      http://timeserver.i.sigmaukraine.com/accountreport.ets
+// @match      https://timeserver.i.sigmaukraine.com/accountreport.ets
+// @copyright  2014+, alreva
+
+// @grant       GM_xmlhttpRequest
+
+// @require		https://code.jquery.com/jquery-2.1.1.min.js
+// @require		http://courses.ischool.berkeley.edu/i290-4/f09/resources/gm_jq_xhr.js
+// @require		https://raw.githubusercontent.com/alreva/ets/master/TamperMonkey/ets.js
+// @require		https://raw.githubusercontent.com/alreva/ets/master/TamperMonkey/ets-tfs.js
+
+// ==/UserScript==
