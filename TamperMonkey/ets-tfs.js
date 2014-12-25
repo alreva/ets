4f523b9a6e4d0dd751feb48168cbc6a764fc4c07
alreva@gmail.com <ise6Songi>
2014-12-25 09:58:37 +0200

    autoupdate getscript reference fixes

--- TamperMonkey/ets-tfs.js
+++ TamperMonkey/ets-tfs.js
@@ -127,93 +127,93 @@ Prerequisites
             $descTd.connectionFailedMessage();
         }
     });
+});
 
-    (function () {
+(function () {
 
-        var $proj = $('#effortRecordProjectCode');
-        var $task = $('#effortRecordIssueCode');
-        var $time = $('[name="effortRecordEffort"]');
-        var $ot = $('[name="effortRecordEffortOvertime"]');
-        var $desc = $('[name="effortRecordDescription"]');
-        var $costC = $('#costCenterAccount');
+    var $proj = $('#effortRecordProjectCode');
+    var $task = $('#effortRecordIssueCode');
+    var $time = $('[name="effortRecordEffort"]');
+    var $ot = $('[name="effortRecordEffortOvertime"]');
+    var $desc = $('[name="effortRecordDescription"]');
+    var $costC = $('#costCenterAccount');
 
-        var pasteRecord = function (r) {
+    var pasteRecord = function (r) {
 
-            $proj.val(r.p);
-            $time.val(r.h);
-            $ot.val(r.o);
-            $desc.val(r.d);
+        $proj.val(r.p);
+        $time.val(r.h);
+        $ot.val(r.o);
+        $desc.val(r.d);
 
-            if (r.c) {
-                $costC.val(r.c);
-            }
-
-            $proj.change();
-
-            setTimeout(function () {
-                var $selectedOption = $('option:contains("' + r.t + '")');
-                $task.val($selectedOption.val());
-                $task.change();
-            }, 1000);
-        };
-
-        $.fn.etsShortcut = function () {
-
-            $(this).click(function () {
-
-                var $shortcut = $(this);
-
-                pasteRecord({
-                    p: $shortcut.data('p'),
-                    h: $shortcut.data('h'),
-                    o: $shortcut.data('o'),
-                    d: unescape($shortcut.data('d')),
-                    c: $shortcut.data('c'),
-                    t: $shortcut.data('t')
-                });
-            });
-        };
-
-        $.fn.tfsAuthLink = function () {
-            $(this).append('Authenticate by following <a href="http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/EPC%202%20Project%20Board/_backlogs">this link</a> to be able to copy-paste from TFS');
-        };
-
-        $.fn.connectionFailedMessage = function () {
-            $(this).append('!Failed to connect to TFS. Please check if you have VPN connection enabled and the  (<a href="http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop">TFS Web</a>) is accessible.');
-        };
-
-        var tfsActionMappings = [
-              { pattern: 'Architecture artifacts', task: 'DEV - Architecture Artifacts' }
-            , { pattern: 'Home page - Provide a link from ROW catalog to selection or home page', task: 'DEV - Home: Link ROW' }
-            , { pattern: 'Set Model / Serial Number filters in URL', task: 'DEV - Home: Set Model' }
-            , { pattern: 'Catalog POC Prices', task: 'DEV - Ctlg: Price POC' }
-            , { pattern: 'Catalog - Calculate price based on price calculation strategy', task: 'DEV - Ctlg: Price Calc Strategy' }
-            , { pattern: 'Prepared Search', task: 'DEV - Ctlg: Search' }
-            , { pattern: 'Cart - POC Prices', task: 'DEV - Cart: POC Price' }
-            , { pattern: 'Print cart', task: 'DEV - Cart: Print' }
-            , { pattern: 'Export cart', task: 'DEV - Cart: Export' }
-            , { pattern: 'US Checkout', task: 'DEV - Checkout' }
-            , { pattern: 'Bulletins', task: 'DEV - Bulletins' }
-            , { pattern: 'Browsers', task: 'DEV - Browsers' }
-            , { pattern: 'Catalog - Accept links with filters from VPPN', task: 'DEV - Ctlg: Filters' }
-            , { pattern: 'Add parts w/o prices', task: 'DEV - Cart: No Prices' }
-            , { pattern: 'Catalog - Hide inventory', task: 'DEV - Ctlg: Hide Inventory' }
-            , { pattern: 'Kits', task: 'DEV - Ctlg: Product Details Kits' }
-            , { pattern: 'User recognition', task: 'DEV - User Recognition' }
-            , { pattern: 'Select parts from images', task: 'Dev - Ctlg: Select Parts From Images' }
-            , { pattern: 'Unplanned changes', task: 'Unplanned Changes - Nov 2014' }
-        ];
-
-        $.findTask = function (itemTitle) {
-            var taskCandidates = tfsActionMappings.filter(function (elm) {
-                return itemTitle.indexOf(elm.pattern) >= 0;
-            });
-
-            return taskCandidates.length > 0
-                ? taskCandidates[0].task
-                : '';
+        if (r.c) {
+            $costC.val(r.c);
         }
 
+        $proj.change();
 
-    })(jQuery);
-});
+        setTimeout(function () {
+            var $selectedOption = $('option:contains("' + r.t + '")');
+            $task.val($selectedOption.val());
+            $task.change();
+        }, 1000);
+    };
+
+    $.fn.etsShortcut = function () {
+
+        $(this).click(function () {
+
+            var $shortcut = $(this);
+
+            pasteRecord({
+                p: $shortcut.data('p'),
+                h: $shortcut.data('h'),
+                o: $shortcut.data('o'),
+                d: unescape($shortcut.data('d')),
+                c: $shortcut.data('c'),
+                t: $shortcut.data('t')
+            });
+        });
+    };
+
+    $.fn.tfsAuthLink = function () {
+        $(this).append('Authenticate by following <a href="http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/EPC%202%20Project%20Board/_backlogs">this link</a> to be able to copy-paste from TFS');
+    };
+
+    $.fn.connectionFailedMessage = function () {
+        $(this).append('!Failed to connect to TFS. Please check if you have VPN connection enabled and the  (<a href="http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop">TFS Web</a>) is accessible.');
+    };
+
+    var tfsActionMappings = [
+          { pattern: 'Architecture artifacts', task: 'DEV - Architecture Artifacts' }
+        , { pattern: 'Home page - Provide a link from ROW catalog to selection or home page', task: 'DEV - Home: Link ROW' }
+        , { pattern: 'Set Model / Serial Number filters in URL', task: 'DEV - Home: Set Model' }
+        , { pattern: 'Catalog POC Prices', task: 'DEV - Ctlg: Price POC' }
+        , { pattern: 'Catalog - Calculate price based on price calculation strategy', task: 'DEV - Ctlg: Price Calc Strategy' }
+        , { pattern: 'Prepared Search', task: 'DEV - Ctlg: Search' }
+        , { pattern: 'Cart - POC Prices', task: 'DEV - Cart: POC Price' }
+        , { pattern: 'Print cart', task: 'DEV - Cart: Print' }
+        , { pattern: 'Export cart', task: 'DEV - Cart: Export' }
+        , { pattern: 'US Checkout', task: 'DEV - Checkout' }
+        , { pattern: 'Bulletins', task: 'DEV - Bulletins' }
+        , { pattern: 'Browsers', task: 'DEV - Browsers' }
+        , { pattern: 'Catalog - Accept links with filters from VPPN', task: 'DEV - Ctlg: Filters' }
+        , { pattern: 'Add parts w/o prices', task: 'DEV - Cart: No Prices' }
+        , { pattern: 'Catalog - Hide inventory', task: 'DEV - Ctlg: Hide Inventory' }
+        , { pattern: 'Kits', task: 'DEV - Ctlg: Product Details Kits' }
+        , { pattern: 'User recognition', task: 'DEV - User Recognition' }
+        , { pattern: 'Select parts from images', task: 'Dev - Ctlg: Select Parts From Images' }
+        , { pattern: 'Unplanned changes', task: 'Unplanned Changes - Nov 2014' }
+    ];
+
+    $.findTask = function (itemTitle) {
+        var taskCandidates = tfsActionMappings.filter(function (elm) {
+            return itemTitle.indexOf(elm.pattern) >= 0;
+        });
+
+        return taskCandidates.length > 0
+            ? taskCandidates[0].task
+            : '';
+    }
+
+
+})($);
--- TamperMonkey/ets.js
+++ TamperMonkey/ets.js
@@ -46,205 +46,205 @@ $(function () {
     $('.calendar_icon').closest('TD').css('vertical-align', 'top');
 
     $('#acceptFormTable').etsTotals();
-
-    (function () {
-
-        var $proj = $('#effortRecordProjectCode');
-        var $task = $('#effortRecordIssueCode');
-        var $time = $('[name="effortRecordEffort"]');
-        var $ot = $('[name="effortRecordEffortOvertime"]');
-        var $desc = $('[name="effortRecordDescription"]');
-        var $costC = $('#costCenterAccount');
-
-        $.fn.etsShortcut = function () {
-
-            $(this).click(function () {
-
-                var $shortcut = $(this);
-
-                pasteRecord({
-                    p: $shortcut.data('p'),
-                    h: $shortcut.data('h'),
-                    o: $shortcut.data('o'),
-                    d: unescape($shortcut.data('d')),
-                    c: $shortcut.data('c'),
-                    t: $shortcut.data('t')
-                });
-            });
-
-        };
-
-        $.fn.etsCopy = function () {
-
-            $(this).click(function () {
-
-                var $btn = $(this);
-                var $row = $btn.closest('tr');
-
-                pasteRecord({
-                    p: $row.find('td:nth-child(5)').text().trim(),
-                    t: $row.find('td.TableCellContent.tooltip').text().trim(),
-                    h: $row.find('td:nth-child(8)').text().trim(),
-                    o: $row.find('td:nth-child(9)').text().trim(),
-                    d: getDescriptionFromRow($row),
-                    c: ''
-                });
-
-                return false;
-            });
-        };
-
-        $.fn.etsDateButtons = function () {
-            $(this).each(function () {
-                var $datePicker = $(this);
-                var $parent = $datePicker.parent();
-                var dmy = $datePicker.val().split('.');
-                var d = new Date(dmy[2], parseInt(dmy[1]) - 1, dmy[0]);
-
-                $.each([['Mon', 1], ['Tue', 2], ['Wed', 3], ['Thu', 4], ['Fri', 5]], function (i, v) {
-                    $('<button style="width:71px;display:block;">' + v[0] + '</button>')
-                    .click(function () {
-                        $datePicker.val('' + (d.getDate() - d.getDay() + v[1]) + '.' + (d.getMonth() + 1) + '.' + d.getFullYear());
-                        $datePicker.change();
-                        return false;
-                    })
-                    .appendTo($parent);
-                });
-            });
-        };
-
-        $.fn.etsTimeButtons = function () {
-
-            $(this).each(function () {
-                var $btn = $(this);
-                var $parent = $btn.parent();
-
-                $parent.append($('<br />'));
-
-                $('<button style="width:18px;">-</button>')
-                .click(function () {
-                    var v = parseFloat($btn.val()) - 0.5;
-                    $btn.val(v > 0 ? v : 0);
-                    return false;
-                })
-                .appendTo($parent);
-
-                $('<button style="width:18px;">+</button>')
-                .click(function () {
-                    var v = parseFloat($btn.val()) + 0.5;
-                    $btn.val(v > 0 ? v : 0.5);
-                    return false;
-                })
-                .appendTo($parent);
-
-                $.each([0, 0.5, 1, 2, 4], function (i, v) {
-                    $('<button style="width:36px;display:block;text-align:right;">' + v + '</button>')
-                    .click(function () {
-                        $btn.val(v);
-                        return false;
-                    })
-                    .appendTo($parent);
-                });
-            });
-        };
-
-        $.fn.etsTotals = function () {
-
-            var $etsLines = $(this).find('> tbody > tr')
-                .filter(function () {
-                    return !$(this).hasClass('TableRowHeader')
-                    && !$(this).hasClass('TableRowAddNew')
-                    && !$(this).hasClass('TableRowTotal');
-                });
-
-            var allDates = [];
-            var totals = {};
-
-            $etsLines.each(function () {
-                var $row = $(this);
-
-                var dateVal = $row.find('> td:nth-child(12)').text().trim();
-                allDates.push(dateVal);
-
-                var hoursVal = parseFloat($row.find('td:nth-child(8)').text().trim());
-
-                $row
-                    .attr('data-date', dateVal)
-                    .attr('data-hours', hoursVal);
-
-                totals[dateVal] = {
-                    date: dateVal,
-                    total: (totals[dateVal] ? totals[dateVal].total : 0) + (hoursVal || 0)
-                };
-            });
-
-            $.each(totals, function () {
-                if (this.total >= 8.0) {
-                    $etsLines.filter('[data-date="' + this.date + '"]').addClass('ets-day-filled');
-                } else {
-                    $etsLines.filter('[data-date="' + this.date + '"]').first()
-                        .addClass('ets-day-not-filled-top');
-                    var $headTd = $('[data-date="' + this.date + '"] > TD').eq(1);
-                    $headTd.html($headTd.html() + ' ' + this.total + " / 8")
-                    $etsLines.filter('[data-date="' + this.date + '"]').last().addClass('ets-day-not-filled-bottom');
-                }
-            });
-
-            var customStyle =
-                ".ets-day-filled {background-color:#dff0d8} " +
-                ".ets-day-not-filled-top TD {border-top: 1px dotted #999;} " +
-                ".ets-day-not-filled-bottom TD {border-bottom: 1px dotted #999} " +
-                ".ets-day-not-filled-top TD:nth-child(2) {text-align: right; font-weight:bold; color:#eea236} "
-            ;
-
-            addGlobalStyle(customStyle);
-        }
-
-        var pasteRecord = function (r) {
-
-            $proj.val(r.p);
-            $time.val(r.h);
-            $ot.val(r.o);
-            $desc.val(r.d);
-
-            if (r.c) {
-                $costC.val(r.c);
-            }
-
-            $proj.change();
-
-            /*        $.get("http://timeserver/ajax/issuedropdawn.ets?effortRecordProjectCode=" + r.p + "&_=", function(data) {
-                        debugger;
-                    });*/
-
-            setTimeout(function () {
-                var $selectedOption = $('option:contains("' + r.t + '")');
-                $task.val($selectedOption.val());
-                $task.change();
-            }, 1000);
-        };
-
-        var getDescriptionFromRow = function ($row) {
-
-            var fullDescription = $row.find('td.TableCellContent.whsp_normal div[id^="hintabletextfull"]').text().trim();
-
-            if (fullDescription) { return fullDescription; }
-
-            return $row.find('td.TableCellContent.whsp_normal div').text().trim();
-        }
-
-        var addGlobalStyle = function (css) {
-            var head, style;
-            head = document.getElementsByTagName('head')[0];
-            if (!head) { return; }
-            style = document.createElement('style');
-            style.type = 'text/css';
-            style.innerHTML = css;
-            head.appendChild(style);
-        }
-
-    })(jQuery);
 });
+
+(function ($) {
+
+    var $proj = $('#effortRecordProjectCode');
+    var $task = $('#effortRecordIssueCode');
+    var $time = $('[name="effortRecordEffort"]');
+    var $ot = $('[name="effortRecordEffortOvertime"]');
+    var $desc = $('[name="effortRecordDescription"]');
+    var $costC = $('#costCenterAccount');
+
+    $.fn.etsShortcut = function () {
+
+        $(this).click(function () {
+
+            var $shortcut = $(this);
+
+            pasteRecord({
+                p: $shortcut.data('p'),
+                h: $shortcut.data('h'),
+                o: $shortcut.data('o'),
+                d: unescape($shortcut.data('d')),
+                c: $shortcut.data('c'),
+                t: $shortcut.data('t')
+            });
+        });
+
+    };
+
+    $.fn.etsCopy = function () {
+
+        $(this).click(function () {
+
+            var $btn = $(this);
+            var $row = $btn.closest('tr');
+
+            pasteRecord({
+                p: $row.find('td:nth-child(5)').text().trim(),
+                t: $row.find('td.TableCellContent.tooltip').text().trim(),
+                h: $row.find('td:nth-child(8)').text().trim(),
+                o: $row.find('td:nth-child(9)').text().trim(),
+                d: getDescriptionFromRow($row),
+                c: ''
+            });
+
+            return false;
+        });
+    };
+
+    $.fn.etsDateButtons = function () {
+        $(this).each(function () {
+            var $datePicker = $(this);
+            var $parent = $datePicker.parent();
+            var dmy = $datePicker.val().split('.');
+            var d = new Date(dmy[2], parseInt(dmy[1]) - 1, dmy[0]);
+
+            $.each([['Mon', 1], ['Tue', 2], ['Wed', 3], ['Thu', 4], ['Fri', 5]], function (i, v) {
+                $('<button style="width:71px;display:block;">' + v[0] + '</button>')
+                .click(function () {
+                    $datePicker.val('' + (d.getDate() - d.getDay() + v[1]) + '.' + (d.getMonth() + 1) + '.' + d.getFullYear());
+                    $datePicker.change();
+                    return false;
+                })
+                .appendTo($parent);
+            });
+        });
+    };
+
+    $.fn.etsTimeButtons = function () {
+
+        $(this).each(function () {
+            var $btn = $(this);
+            var $parent = $btn.parent();
+
+            $parent.append($('<br />'));
+
+            $('<button style="width:18px;">-</button>')
+            .click(function () {
+                var v = parseFloat($btn.val()) - 0.5;
+                $btn.val(v > 0 ? v : 0);
+                return false;
+            })
+            .appendTo($parent);
+
+            $('<button style="width:18px;">+</button>')
+            .click(function () {
+                var v = parseFloat($btn.val()) + 0.5;
+                $btn.val(v > 0 ? v : 0.5);
+                return false;
+            })
+            .appendTo($parent);
+
+            $.each([0, 0.5, 1, 2, 4], function (i, v) {
+                $('<button style="width:36px;display:block;text-align:right;">' + v + '</button>')
+                .click(function () {
+                    $btn.val(v);
+                    return false;
+                })
+                .appendTo($parent);
+            });
+        });
+    };
+
+    $.fn.etsTotals = function () {
+
+        var $etsLines = $(this).find('> tbody > tr')
+            .filter(function () {
+                return !$(this).hasClass('TableRowHeader')
+                && !$(this).hasClass('TableRowAddNew')
+                && !$(this).hasClass('TableRowTotal');
+            });
+
+        var allDates = [];
+        var totals = {};
+
+        $etsLines.each(function () {
+            var $row = $(this);
+
+            var dateVal = $row.find('> td:nth-child(12)').text().trim();
+            allDates.push(dateVal);
+
+            var hoursVal = parseFloat($row.find('td:nth-child(8)').text().trim());
+
+            $row
+                .attr('data-date', dateVal)
+                .attr('data-hours', hoursVal);
+
+            totals[dateVal] = {
+                date: dateVal,
+                total: (totals[dateVal] ? totals[dateVal].total : 0) + (hoursVal || 0)
+            };
+        });
+
+        $.each(totals, function () {
+            if (this.total >= 8.0) {
+                $etsLines.filter('[data-date="' + this.date + '"]').addClass('ets-day-filled');
+            } else {
+                $etsLines.filter('[data-date="' + this.date + '"]').first()
+                    .addClass('ets-day-not-filled-top');
+                var $headTd = $('[data-date="' + this.date + '"] > TD').eq(1);
+                $headTd.html($headTd.html() + ' ' + this.total + " / 8")
+                $etsLines.filter('[data-date="' + this.date + '"]').last().addClass('ets-day-not-filled-bottom');
+            }
+        });
+
+        var customStyle =
+            ".ets-day-filled {background-color:#dff0d8} " +
+            ".ets-day-not-filled-top TD {border-top: 1px dotted #999;} " +
+            ".ets-day-not-filled-bottom TD {border-bottom: 1px dotted #999} " +
+            ".ets-day-not-filled-top TD:nth-child(2) {text-align: right; font-weight:bold; color:#eea236} "
+        ;
+
+        addGlobalStyle(customStyle);
+    }
+
+    var pasteRecord = function (r) {
+
+        $proj.val(r.p);
+        $time.val(r.h);
+        $ot.val(r.o);
+        $desc.val(r.d);
+
+        if (r.c) {
+            $costC.val(r.c);
+        }
+
+        $proj.change();
+
+        /*        $.get("http://timeserver/ajax/issuedropdawn.ets?effortRecordProjectCode=" + r.p + "&_=", function(data) {
+                    debugger;
+                });*/
+
+        setTimeout(function () {
+            var $selectedOption = $('option:contains("' + r.t + '")');
+            $task.val($selectedOption.val());
+            $task.change();
+        }, 1000);
+    };
+
+    var getDescriptionFromRow = function ($row) {
+
+        var fullDescription = $row.find('td.TableCellContent.whsp_normal div[id^="hintabletextfull"]').text().trim();
+
+        if (fullDescription) { return fullDescription; }
+
+        return $row.find('td.TableCellContent.whsp_normal div').text().trim();
+    }
+
+    var addGlobalStyle = function (css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+
+})($);
 
 
 
