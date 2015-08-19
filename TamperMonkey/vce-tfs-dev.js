67d8fd218d8b7ee6769064ffeee8ee88d6ea4e13
alreva <alreva@gmail.com>
2015-08-19 19:16:24 +0300

    Adde "dev" versions of the files

---
+++ TamperMonkey/vce-tfs-dev.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @name         [DEV] VCE TFS ETS Reporting
+// @namespace    http://timeserver
+// @version      0.1
+// @description  this one helps properly reporting ETS with TFS items.
+// @author       areva
+// @match        timeserver/accountreport.ets
+// @match        ets.sigmaukraine.com:7443/accountreport.ets
+
+// @grant       GM_xmlhttpRequest
+
+
+// @require		https://code.jquery.com/jquery-2.1.4.min.js
+// @require		http://courses.ischool.berkeley.edu/i290-4/f09/resources/gm_jq_xhr.js
+// @require		file://C:\Users\areva\Documents\Visual Studio 2013\Projects\ETSImprove\TamperMonkey\vce-tfs.js
+
+// ==/UserScript==
