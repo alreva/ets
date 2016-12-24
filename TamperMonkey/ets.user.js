133b53f4a978fe54643f4e29909cf1e88e8035a3
alreva <alreva@gmail.com>
2016-12-24 14:03:11 +0200

    Changed how chosen works

--- TamperMonkey/ets.user.js
+++ TamperMonkey/ets.user.js
@@ -1,67 +1,162 @@
 ﻿// ==UserScript==
-
 // @name         ETS Improvements
 // @namespace    http://timeserver
 // @version      0.1
 // @description  this one helps filling out ETS faster.
 // @author       areva
-// @match        timeserver/accountreport.ets
-// @match        ets.sigmaukraine.com:7443/accountreport.ets
+// @match        timeserver/timereports.ets
+// @match        timeserver.i.sigmaukraine.com/timereports.ets
+// @match        ets.sigmaukraine.com:7443/timereports.ets
 
 // @grant        GM_xmlhttpRequest
 
-// @require      https://code.jquery.com/jquery-2.1.4.min.js
+
+// reference:    https://harvesthq.github.io/chosen/
+// @require      https://code.jquery.com/jquery-2.2.4.min.js
+// @require		 https://cdnjs.cloudflare.com/ajax/libs/chosen/1.6.2/chosen.jquery.min.js
+
 // @require		 http://courses.ischool.berkeley.edu/i290-4/f09/resources/gm_jq_xhr.js
 
 // ==/UserScript==
 
-(function ($) {
-    var $proj = $('#effortRecordProjectCode');
-    var $task = $('#effortRecordIssueCode');
-    var $time = $('[name="effortRecordEffort"]');
-    var $ot = $('[name="effortRecordEffortOvertime"]');
-    var $desc = $('[name="effortRecordDescription"]');
-    var $costC = $('#costCenterAccount');
-    var getTagSelect = function() { return $('select.tag') };
+const loadJsCss = (filename, filetype) => {
+    var fileref = "";
+    if (filetype == "js") {
+        fileref = document.createElement('script')
+        fileref.setAttribute("type", "text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype == "css") {
+        fileref = document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref != "undefined")
+        document.getElementsByTagName("body")[0].appendChild(fileref);
+}
 
+const loadInlineCss = (css) => {
+    $('<style type="text/css"></style>')
+        .html(css)
+        .appendTo("body");
+}
 
-    var pasteRecord = function (r) {
+const notifyOptionAdded = ($option) => {
+    var $parent = $option.closest('select');
+    $parent.trigger('chosen:updated');
+}
 
-        $proj.val(r.p);
-        $time.val(r.h);
-        $ot.val(r.o);
-        $desc.val(r.d);
+const wrapWithChosen = () => {
 
-        if (r.c) {
-            $costC.val(r.c);
+    loadJsCss("https://cdnjs.cloudflare.com/ajax/libs/chosen/1.6.2/chosen.min.css", "css");
+
+    loadInlineCss(`
+
+.chosen-container .chosen-drop {width: 400px;}
+.chosen-container-single .chosen-single {border-radius:0; font-weight:normal;text-transform:none;}
+.chosen-container .chosen-results li {padding:0;text-transform:none;}
+
+        `);
+
+    const wrapSelectWithChosen = ($select) => {
+        $select.chosen({
+            disable_search_threshold: 10,
+            search_contains: true
+        });
+
+        $select.on('change', (evt, params) => {
+            $('select').trigger('chosen:updated');
+        });
+    }
+
+    wrapSelectWithChosen($('select:visible')
+        // cost cender, by some reason, has 'visibility:hodden' attribute instead of simply being hidden. 
+        .not('#costCenterAccount'));
+
+    $(document).on("DOMNodeInserted", (evt) => {
+        if (evt.target && evt.target.tagName === 'SELECT') {
+            wrapSelectWithChosen($(evt.target).filter(':visible'));
         }
 
-        $proj.change();
+        if (evt.target && evt.target.tagName === 'OPTION') {
+            notifyOptionAdded($(evt.target));
+        }
+    });
+}
 
-        setTimeout(function () {
+const nav = {
+    project: () => $('#projectCode select'),
+    task: () => $('#issueCode select'),
+    tag: () => $('select.tag'),
+    time: () => $('[name="effortRecordEffort"]'),
+    overtime: () => $('[name="effortRecordEffortOvertime"]'),
+    description: () => $('[name="effortRecordDescription"]'),
+    costCenter: () => $('#costCenterAccount')
+}
 
-            var $selectedTaskOption = $('option:contains("' + r.t + '")');
-            $task.val($selectedTaskOption.val());
+const notifyElementChange = (element) => {
+    const changeEvent = document.createEvent("HTMLEvents");
+    changeEvent.initEvent("change", true, true);
+    element.dispatchEvent(changeEvent);
+}
 
-            if (r.tag) {
-                var $selectedTagOption = $('select.tag option:contains("' + r.tag + '")');
-                var $tagSelect = $('select.tag');
-				$tagSelect.val($selectedTagOption.val());
-				var changeEvent = document.createEvent("HTMLEvents");
-				changeEvent.initEvent("change", true, true);
-				$tagSelect[0].dispatchEvent(changeEvent);
-				
-				//var evt = $.Event("change", {"bubbles": true, "cancelable": true});
-				//$tagSelect.trigger(evt);
-				
-				//$tagSelect.change();
+const listenToSelectOptionAdd = (select, addHandler) => {
+
+    const evtHandler = (evt) => {
+        if (evt.target && evt.target.tagName == 'OPTION') {
+            const actualSelect = $(evt.target).closest('select')[0];
+            if (actualSelect == select) {
+                const added = addHandler(select, evt.target);
+                if (added) {
+                    $('body').off('DOMNodeInserted', evtHandler);
+                }
             }
+        }
+    }
 
-            $task.change();
-        }, 1000);
+    $('body').on('DOMNodeInserted', evtHandler);
+}
+
+const setSelectValueAndNotifyChanged = ($select, val) => {
+    listenToSelectOptionAdd($select[0], (select, option) => {
+        const optionFound = option.value == val || option.innerText == val;
+        if (optionFound) {
+            $(select).val(option.value);
+        }
+        notifyOptionAdded($(select));
+        return optionFound;
+    });
+}
+
+const pasteRecord = ({
+    project,
+    task,
+    tag,
+    time,
+    overtime,
+    description,
+    costCenter
+}) => {
+
+        nav.project().val(project);
+        nav.time().val(time);
+        nav.overtime().val(overtime);
+        nav.description().val(description);
+
+        if (costCenter) {
+            nav.costCenter().val(costCenter);
+        }
+
+        nav.project().change();
+
+        setSelectValueAndNotifyChanged(nav.task(), task);
+        setSelectValueAndNotifyChanged(nav.tag(), tag);
     };
 
-    var getDescriptionFromRow = function ($row) {
+(() => {
+
+    const getDescriptionFromRow = ($row) => {
 
         var fullDescription = $row.find('td.TableCellContent.whsp_normal div[id^="hintabletextfull"]').text().trim();
 
@@ -72,7 +167,7 @@
         return $row.find('td.TableCellContent.whsp_normal div').text().trim();
     }
 
-    var getTaskFromRow = function ($row) {
+    const getTaskFromRow = ($row) => {
 
         var $taskAndTags = $row.find('td.TableCellContent.tooltip');
         var $copy = $("<div>" + $taskAndTags.html() + "</div>");
@@ -80,7 +175,7 @@
         return $copy.text().trim();
     }
 
-    var getTagFromRow = function ($row, tagName) {
+    const getTagFromRow = ($row, tagName) => {
 
         var tagsStr = $row.find("td.TableCellContent.tooltip span").text().trim();
 
@@ -96,38 +191,6 @@
             : undefined;
     }
 
-    $.addGlobalStyle = function (css) {
-        var head, style;
-        head = document.getElementsByTagName('head')[0];
-        if (!head) {
-            return;
-        }
-        style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = css;
-        head.appendChild(style);
-    }
-
-
-    $.fn.etsShortcut = function () {
-
-        $(this).click(function () {
-
-            var $shortcut = $(this);
-
-            pasteRecord({
-                p: $shortcut.data('p'),
-                h: $shortcut.data('h'),
-                o: $shortcut.data('o'),
-                d: unescape($shortcut.data('d')),
-                c: $shortcut.data('c'),
-                t: $shortcut.data('t'),
-                tag: $shortcut.data('tag')
-            });
-        });
-
-    };
-
     $.fn.etsCopy = function () {
 
         $(this).click(function () {
@@ -136,13 +199,13 @@
             var $row = $btn.closest('tr');
 
             pasteRecord({
-                p: $row.find('td:nth-child(6)').text().trim(),
-                t: getTaskFromRow($row),
-                h: $row.find('td:nth-child(9)').text().trim(),
-                o: $row.find('td:nth-child(10)').text().trim(),
-                d: getDescriptionFromRow($row),
-                c: '',
-                tag: getTagFromRow($row, 'Customer')
+                project: $row.find('td:nth-child(6)').text().trim(),
+                task: getTaskFromRow($row),
+                tag: getTagFromRow($row, 'Customer'),
+                time: $row.find('td:nth-child(9)').text().trim(),
+                overtime: $row.find('td:nth-child(10)').text().trim(),
+                description: getDescriptionFromRow($row),
+                costCenter: ''
             });
 
             return false;
@@ -258,23 +321,51 @@
             }
         });
 
-        var customStyle =
-            ".ets-day-filled {background-color:#dff0d8} " +
-                ".ets-day-not-filled-top TD {border-top: 1px dotted #999;} " +
-                ".ets-day-not-filled-bottom TD {border-bottom: 1px dotted #999} " +
-                ".ets-day-not-filled-top TD:nth-child(2) {text-align: right; font-weight:bold; color:#eea236} ";
+        loadInlineCss(`
+.ets-day-filled:not(.TableRowDeclined) {background-color:#dff0d8}
+.ets-day-not-filled-top TD {border-top: 1px dotted #999;}
+.ets-day-not-filled-bottom TD {border-bottom: 1px dotted #999}
+.ets-day-not-filled-top TD: nth-child(2) {text-align: right; font-weight: bold; color: #eea236}
 
-        $.addGlobalStyle(customStyle);
-    }
+        `);
+
+    };
+
+    /*
+
+    $.fn.etsShortcut = function () {
+
+        $(this).click(function () {
+
+            var $shortcut = $(this);
+
+            pasteRecord({
+                p: $shortcut.data('p'),
+                h: $shortcut.data('h'),
+                o: $shortcut.data('o'),
+                d: unescape($shortcut.data('d')),
+                c: $shortcut.data('c'),
+                t: $shortcut.data('t'),
+                tag: $shortcut.data('tag')
+            });
+        });
+
+    };
 
     var shortcuts = [];
 
-    var $desc = $('[name="effortRecordDescription"]');
+    var $descTd = nav.description().closest('TD');
 
-    var $descTd = $desc.closest('TD');
-
-    var buildShortcutHtml = function (s) {
-        return '<div><a class="ets-shortcut" data-p="' + s.p + '" data-t="' + s.t + '" data-h="' + s.h + '" data-o="' + s.o + '" data-d="' + escape(s.d) + '" data-c="' + s.c + '">' + s.s + '</a></div>';
+    const buildShortcutHtml = (s) => {
+        return `<div><a class="ets-shortcut" 
+        data-p="${s.p}" 
+        data-t="${s.t}" 
+        data-h="${s.h}" 
+        data-o="${s.o}" 
+        data-d="${escape(s.d)}" 
+        data-c="${s.c}">
+            ${s.s}
+        </a></div>`;
     };
 
     $.each(shortcuts, function () {
@@ -283,13 +374,14 @@
 
     $('.ets-shortcut').etsShortcut();
 
+    */
+
     if ($('[name="effortRecordDescription"]').length > 0) {
         $('.TableCellContent.whsp_normal').prepend('<button class="button-copy" style="float:right;">Copy</button>');
         $('.button-copy').etsCopy();
     }
 
     $('[name="effortRecordEffort"], [name="effortRecordEffortOvertime"]').etsTimeButtons();
-
     $('[name="effortRecordStarted"], [name="effortRecordFinished"]').etsDateButtons();
 
     $('[name="effortRecordStarted"]').change(function () {
@@ -300,7 +392,5 @@
 
     $('#acceptFormTable').etsTotals();
 
-})($);
-
-
-
+    wrapWithChosen();
+})();
